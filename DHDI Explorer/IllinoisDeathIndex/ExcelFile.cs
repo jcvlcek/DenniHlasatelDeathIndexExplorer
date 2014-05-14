0b9157a7443b17e57728473d5251fd1c047fa061
jcvlcek <vlcek@msn.com>
2014-05-14 13:15:44 -0500

    Added "Javadoc" comments, obeyed "two-dot rule" for COM objects when accessing Excel methods

--- DHDI Explorer/IllinoisDeathIndex/ExcelFile.cs
+++ DHDI Explorer/IllinoisDeathIndex/ExcelFile.cs
@@ -7,6 +7,9 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Genealogy
 {
+    /// <summary>
+    /// Basic wrapper class for working with Excel files in .NET
+    /// </summary>
     public class ExcelFile
     {
         #region Private members
@@ -16,11 +19,17 @@ namespace Genealogy
         #endregion
 
         #region Constructors
+
+        /// <summary>
+        /// Open an Excel spreadsheet at a specified path
+        /// </summary>
+        /// <param name="sPath">the file path name of the Excel spreadsheet</param>
         public ExcelFile(string sPath)
         {
             if (mXlApp == null)
                 mXlApp = new Excel.Application();
-            mXlWorkBook = mXlApp.Workbooks.Open(sPath,
+            Excel.Workbooks workBooks = mXlApp.Workbooks;
+            mXlWorkBook = workBooks.Open(sPath,
                 Excel.XlUpdateLinks.xlUpdateLinksAlways,
                 true, 
                 5, // Excel.XlFileFormat.xlWorkbookDefault,
@@ -35,16 +44,26 @@ namespace Genealogy
             mXlDefaultWorkSheet = mXlWorkBook.Worksheets[1];
         }
 
+        /// <summary>
+        /// Create an empty Excel spreadsheet, with no associated file
+        /// </summary>
         public ExcelFile()
         {
             if (mXlApp == null)
                 mXlApp = new Excel.Application();
-            mXlWorkBook = mXlApp.Workbooks.Add();
+            Excel.Workbooks workBooks = mXlApp.Workbooks;
+            mXlWorkBook = workBooks.Add();
             mXlDefaultWorkSheet = mXlWorkBook.Worksheets[1];
         }
         #endregion
 
         #region Public methods
+
+        /// <summary>
+        /// Get the string value of a spreadsheet cell's contents
+        /// </summary>
+        /// <param name="where">the row (<paramref name="where"/>.Y) and column (<paramref name="where"/>.X) of the cell to inspect</param>
+        /// <returns>the string value of the contents of the cell at <paramref name="where"/>, or the empty string on error</returns>
         public string ValueAt(Point where)
         {
             try
@@ -57,16 +76,30 @@ namespace Genealogy
             }
         }
 
+        /// <summary>
+        /// Set the value of a spreadsheet cell's contents
+        /// </summary>
+        /// <param name="where">the row (<paramref name="where"/>.Y) and column (<paramref name="where"/>.X) of the cell to inspect</param>
+        /// <param name="sValue">the value to be written to the cell at <paramref name="where"/></param>
         public void SetValueAt(Point where, string sValue)
         {
             mXlDefaultWorkSheet.Cells[where.Y, where.X].Value2 = sValue;
         }
 
+        /// <summary>
+        /// Save the spreadsheet to a specified file path
+        /// </summary>
+        /// <param name="sPath">the file path to which the spreadsheet is to be saved</param>
         public void SaveAs(string sPath)
         {
             mXlWorkBook.SaveAs(sPath, Excel.XlFileFormat.xlExcel8);
         }
 
+        /// <summary>
+        /// Close the spreadsheet
+        /// </summary>
+        /// <remarks><para>No testing for unsaved changes is performed by this method</para>
+        /// This method also attempts to quit the Excel application object</remarks>
         public void Close()
         {
             mXlWorkBook.Close();
