2119f18563b86896e43b76dc7634dde8242904ca
jcvlcek <vlcek@msn.com>
2014-05-20 12:43:45 -0500

    Added XML documentation comments

--- DHDI Explorer/IllinoisDeathIndex/IProperty.cs
+++ DHDI Explorer/IllinoisDeathIndex/IProperty.cs
@@ -5,9 +5,19 @@ using System.Text;
 
 namespace Genealogy
 {
+    /// <summary>
+    /// Interface implemeted by simple, scalar properties
+    /// </summary>
     public interface IProperty
     {
+        /// <summary>
+        /// The (read-only) name of the property
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// The (read/write) value of the property
+        /// </summary>
         string Value { get; set; }
     }
 }
