01bf332cd7c41497af1468e4c5fcaf3139e03f10
jcvlcek <vlcek@msn.com>
2014-06-02 21:52:22 -0500

    Added XML documentation comments

--- DHDI Explorer/IllinoisDeathIndex/ISaveFormat.cs
+++ DHDI Explorer/IllinoisDeathIndex/ISaveFormat.cs
@@ -5,9 +5,24 @@ using System.Text;
 
 namespace Genealogy
 {
+    /// <summary>
+    /// Interface implemented by classes that serialize objects implementing IObject
+    /// </summary>
+    /// <remarks>This interface suggests the Visitor pattern for serializing IObjects</remarks>
     interface ISaveFormat
     {
+        /// <summary>
+        /// Serialize an IObject to a stream
+        /// </summary>
+        /// <param name="stream">the stream to serialize the object to</param>
+        /// <param name="o">the object to be serialized</param>
         void Stream(System.IO.StreamWriter stream, IObject o);
+
+        /// <summary>
+        /// Serialize a property of an IObject to a stream
+        /// </summary>
+        /// <param name="stream">the stream to serialize the property to</param>
+        /// <param name="p">the property to be serialized</param>
         void Stream(System.IO.StreamWriter stream, IProperty p);
     }
 }
