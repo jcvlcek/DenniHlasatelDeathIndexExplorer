12574b59f57f15c05bf1df54513966f15e4bc706
jcvlcek <vlcek@msn.com>
2014-06-03 13:06:35 -0500

    Added XML documentation comments

--- DHDI Explorer/IllinoisDeathIndex/JsonSaveFormat.cs
+++ DHDI Explorer/IllinoisDeathIndex/JsonSaveFormat.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 namespace Genealogy
 {
+    /// <summary>
+    /// IObject serializer for JSON (JavaScript Object Notation) format
+    /// </summary>
     class JsonSaveFormat : ISaveFormat
     {
+        /// <summary>
+        /// Stream an IObject to JSON
+        /// </summary>
+        /// <param name="stream">the stream to serialize the object to</param>
+        /// <param name="o">the oject to be serialized</param>
         public void Stream( System.IO.StreamWriter stream, IObject o )
         {
             try
@@ -22,6 +30,11 @@ namespace Genealogy
             } 
         }
 
+        /// <summary>
+        /// Stream an IProperty to JSON
+        /// </summary>
+        /// <param name="stream">the stream to serialize the property to</param>
+        /// <param name="p">the property to be serialized</param>
         public void Stream(System.IO.StreamWriter stream, IProperty p)
         {
             try
