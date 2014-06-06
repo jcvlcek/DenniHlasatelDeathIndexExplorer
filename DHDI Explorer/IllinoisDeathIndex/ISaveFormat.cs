775c080135e8c7d702426418ec1fe69eecab0c58
jcvlcek <vlcek@msn.com>
2014-06-05 20:52:47 -0500

    Removed unused "using" directives

--- DHDI Explorer/IllinoisDeathIndex/ISaveFormat.cs
+++ DHDI Explorer/IllinoisDeathIndex/ISaveFormat.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Genealogy
+﻿namespace Genealogy
 {
     /// <summary>
     /// Interface implemented by classes that serialize objects implementing IObject
