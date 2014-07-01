b1fbf3755637bdddf3910fd2877cddcbd01d1767
jcvlcek <vlcek@msn.com>
2014-07-01 12:52:31 -0500

    Refactoring a la ReSharper

--- DHDI Explorer/IllinoisDeathIndex/GivenName.cs
+++ DHDI Explorer/IllinoisDeathIndex/GivenName.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using dbAccess;
 
 namespace Genealogy
@@ -16,15 +15,6 @@ namespace Genealogy
 
         #region Private members
 
-        /// <summary>
-        /// Count of males in the reference census with this given name
-        /// </summary>
-        private int mMaleCount = 0;
-
-        /// <summary>
-        /// Count of females in the reference census with this given name
-        /// </summary>
-        private int mFemaleCount = 0;
         #endregion
 
         #region Constructors
@@ -36,6 +26,8 @@ namespace Genealogy
         /// <param name="sName">the native (code page) form of the name</param>
         private GivenName(string sName) : base( sName )
         {
+            FemaleCount = 0;
+            MaleCount = 0;
             var queryNative = (from cNative in Utilities.dB.KrestniJmenas
                                where cNative.CodePage == sName
                                select cNative).FirstOrDefault();
@@ -81,20 +73,12 @@ namespace Genealogy
         /// <summary>
         /// Total number of males in the census with this given name
         /// </summary>
-        public int MaleCount
-        {
-            get { return mMaleCount; }
-            private set { mMaleCount = value; }
-        }
+        public int MaleCount { get; private set; }
 
         /// <summary>
         /// Total number of females in the census with this given name
         /// </summary>
-        public int FemaleCount
-        {
-            get { return mFemaleCount; }
-            private set { mFemaleCount = value; }
-        }
+        public int FemaleCount { get; private set; }
 
         /// <summary>
         /// Total number of individuals in the census with this given name
@@ -108,75 +92,75 @@ namespace Genealogy
         /// A comprehensive list of all available alternate forms of this name.
         /// This includes forms that match either a plain text or a "native" spelling of the name.
         /// The returned list will always have at least one entry, and the first entry in the list
-        /// will always have a <see cref="NativeForm.PlainText"/> property equal to <see cref="Value"/>
+        /// will always have a <see cref="PersonName{T}.NativeForm.PlainText"/> property equal to <see cref="PersonName{T}.NativeForm.Value"/>
         /// </summary>
         public override List<NativeForm> AlternateForms
         {
             get
             {
-                if (mAlternateForms == null)
+                if (mAlternateForms != null) return mAlternateForms;
+
+                mAlternateForms = new List<NativeForm>();
+
+                // First, assemble a query to get all direct matches
+                var query1 = from c in Utilities.dB.KrestniJmenas
+                    where (c.PlainText == Value) || (c.CodePage == Value)
+                    select c;
+
+                // Add all of the "hits" on the direct match query
+                foreach (KrestniJmena qNext in query1)
                 {
-                    mAlternateForms = new List<NativeForm>();
+                    mAlternateForms.Add(new NativeForm(qNext.CodePage, qNext.Web, qNext.PlainText));
+                }
 
-                    // First, assemble a query to get all direct matches
-                    var query1 = from c in Utilities.dB.KrestniJmenas
-                                 where (c.PlainText == Value) || (c.CodePage == Value)
-                                 select c;
+                // If no direct matches found, add a record for the original value
+                // with the native form set to an empty string to flag 
+                // that there is no direct equivalent
+                if (mAlternateForms.Count < 1)
+                    mAlternateForms.Add(new NativeForm(string.Empty, Value, Value));
 
-                    // Add all of the "hits" on the direct match query
-                    foreach (KrestniJmena qNext in query1)
+                var lNatives = new List<String>();
+                var lPlainTexts = new List<string>();
+
+                // Organize lists of matches for native and plain text forms of the name
+                // based on the original query
+                foreach (NativeForm nfNext in mAlternateForms)
+                {
+                    if (!lNatives.Contains(nfNext.Value))
+                        lNatives.Add(nfNext.Value);
+                    if (!lPlainTexts.Contains(nfNext.PlainText))
+                        lPlainTexts.Add(nfNext.PlainText);
+                }
+
+                // Construct the query to get the "first level" matches
+                var query2 = from c in Utilities.dB.GivenNameEquivalents
+                    where lNatives.Contains(c.Native) || lPlainTexts.Contains(c.English)
+                    select c;
+
+                // Add all the "hits" on the indirect query
+                foreach (GivenNameEquivalent geNext in query2)
+                {
+                    GivenNameEquivalent next = geNext;
+                    var query3 = from c in Utilities.dB.KrestniJmenas
+                        where c.CodePage == next.Native
+                        select c;
+
+                    string sWeb = query3.Any() ? query3.First().Web : geNext.English;
+                    var nfNew = new NativeForm(geNext.Native, geNext.English, sWeb);
+                    if (!mAlternateForms.Contains(nfNew))
                     {
-                        mAlternateForms.Add(new NativeForm(qNext.CodePage, qNext.Web, qNext.PlainText));
-                    }
+                        mAlternateForms.Add(nfNew);
 
-                    // If no direct matches found, add a record for the original value
-                    // with the native form set to an empty string to flag 
-                    // that there is no direct equivalent
-                    if (mAlternateForms.Count < 1)
-                        mAlternateForms.Add(new NativeForm(string.Empty, Value, Value));
-
-                    List<String> lNatives = new List<String>();
-                    List<string> lPlainTexts = new List<string>();
-
-                    // Organize lists of matches for native and plain text forms of the name
-                    // based on the original query
-                    foreach (NativeForm nfNext in mAlternateForms)
-                    {
-                        if (!lNatives.Contains(nfNext.Value))
-                            lNatives.Add(nfNext.Value);
-                        if (!lPlainTexts.Contains(nfNext.PlainText))
-                            lPlainTexts.Add(nfNext.PlainText);
-                    }
-
-                    // Construct the query to get the "first level" matches
-                    var query2 = from c in Utilities.dB.GivenNameEquivalents
-                                 where lNatives.Contains(c.Native) || lPlainTexts.Contains(c.English)
-                                 select c;
-
-                    // Add all the "hits" on the indirect query
-                    foreach (GivenNameEquivalent geNext in query2)
-                    {
-                        var query3 = from c in Utilities.dB.KrestniJmenas
-                                     where c.CodePage == geNext.Native
-                                     select c;
-
-                        string sWeb = query3.Count() > 0 ? query3.First().Web : geNext.English;
-                        NativeForm nfNew = new NativeForm(geNext.Native, geNext.English, sWeb);
-                        if (!mAlternateForms.Contains(nfNew))
+                        foreach (KrestniJmena kjNext in query3)
                         {
-                            mAlternateForms.Add(nfNew);
-
-                            foreach (KrestniJmena kjNext in query3)
-                            {
-                                NativeForm nfRecursive = new NativeForm(kjNext.CodePage, kjNext.PlainText, kjNext.Web);
-                                if (!mAlternateForms.Contains(nfRecursive))
-                                    mAlternateForms.Add(nfRecursive);
-                            }
+                            var nfRecursive = new NativeForm(kjNext.CodePage, kjNext.PlainText, kjNext.Web);
+                            if (!mAlternateForms.Contains(nfRecursive))
+                                mAlternateForms.Add(nfRecursive);
                         }
                     }
-
-                    mAlternateForms = mAlternateForms.Distinct().ToList();
                 }
+
+                mAlternateForms = mAlternateForms.Distinct().ToList();
 
                 return mAlternateForms;
             }
@@ -199,17 +183,11 @@ namespace Genealogy
         /// <remarks>Multiple given names may match the same plain-text representation, as a result of differing applications of diacriticals</remarks>
         public static List<GivenName> MatchToPlainTextName(string sPlainTextName, Linq2SqlDataContext mdB)
         {
-            List<GivenName> lMatches = new List<GivenName>();
-
             var qGivenName = from gn in mdB.KrestniJmenas
                              where gn.PlainText == sPlainTextName
                              select gn;
-            foreach (var gnNext in qGivenName)
-            {
-                lMatches.Add(GivenName.Get(gnNext.CodePage));
-            }
 
-            return lMatches;
+            return qGivenName.Select(gnNext => Get(gnNext.CodePage)).ToList();
         }
 
         #endregion
